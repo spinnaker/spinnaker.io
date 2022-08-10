7f1a39e03de4c6de04c62d5e6b7defafa558363d
Steven Zimmer <stevenzimmer40@gmail.com>
2022-08-10 11:55:37 -0700

    docs(infra): add announcement banner (#216)
    * promo banner added above nav on homepage
    
    * promo banner variables added to config toml
    
    * fixed formatting in layouts/head.html
    
    Co-authored-by: aimee <aimeeu.opensource@gmail.com>

---
+++ static/js/promo-banner.js
@@ -0,0 +1,85 @@
+const promoBanner = document.getElementById('promo-banner')
+const closePromoBanner = document.getElementById('close-banner')
+const navbar = document.getElementById('navbar')
+
+if (promoBanner) {
+  console.log('promo banner')
+  closePromoBanner.addEventListener('click', () => {
+    // Close banner
+    promoBanner.classList.remove('active')
+
+    // Drop cookie to keep banner closed 24 hours for user
+    createCookie('banner_closed', 1, 1)
+    navbar.style.top = '0px'
+  })
+
+  if (!readCookie('banner_closed')) {
+    // Show promo banner if no "closed_banner" cookie
+    promoBanner.classList.add('active')
+
+    // Adjust navbar position after promo banner appears
+    setTimeout(() => {
+      navbarPosition()
+    }, 150)
+
+    // Set nav position as user scrolls
+    window.addEventListener('scroll', function () {
+      navbarPosition()
+    })
+
+    // Set nav position as user resizes browser width
+    window.addEventListener('resize', () => {
+      if (window.innerWidth < 992) {
+        navbar.style.top = '0px'
+      } else {
+        navbar.style.top = `${
+          promoBanner.getClientRects()[0].height - this.window.scrollY
+        }px`
+      }
+    })
+  } else {
+    // Set nav position on page load
+    navbarPosition()
+  }
+}
+
+function navbarPosition() {
+  if (this.window.scrollY < promoBanner.offsetHeight) {
+    navbar.style.top = `${
+      promoBanner.getClientRects()[0].height - this.window.scrollY
+    }px`
+  } else {
+    navbar.style.top = '0px'
+  }
+}
+
+// *************************************
+//   Helper function to create cookies **
+// ***************************************
+function createCookie(name, value, days) {
+  var expires = ''
+  if (days) {
+    var date = new Date()
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+    expires = '; expires=' + date.toUTCString()
+  }
+  document.cookie = name + '=' + value + expires + '; path=/'
+}
+
+// **********************************************
+//   Helper function to get/read cookie values  **
+// ************************************************
+function readCookie(name) {
+  var nameEQ = name + '='
+  var ca = document.cookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1, c.length)
+    }
+    if (c.indexOf(nameEQ) == 0) {
+      return c.substring(nameEQ.length, c.length)
+    }
+  }
+  return null
+}
