8ca7073152574c8d92d2824b19cee5f37547f792
Steven Zimmer <webdevzim@gmail.com>
2022-09-07 08:48:55 -0700

    buttons added

---
+++ static/js/ga-events.js
@@ -0,0 +1,8 @@
+const gaBtnTrack = Array.prototype.slice.call(
+  document.querySelectorAll('.ga-btn-track')
+)
+gaBtnTrack.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    ga('send', 'event', e.target.role, e.type, e.target.innerText)
+  })
+})
